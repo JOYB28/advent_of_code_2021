f798afc4e6034a33a0d913ce015dfb2ae4b73462
yoonseo kim <joyn970428@gmail.com>
2022-01-01 23:16:28 +0900

    feat: d1 done

---
+++ src/main/kotlin/d1/Solution.kt
@@ -0,0 +1,21 @@
+package d1
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var ans = 0
+    var prev: Int? = null
+    while (true) {
+        val cur = try {
+            readLine().toInt()
+        } catch (t: Throwable) {
+            break
+        }
+        if (prev != null && prev < cur) {
+            ans += 1
+        }
+        prev = cur
+    }
+    println(ans)
+}
